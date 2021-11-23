4b0b5c37a028f1c025b0c5188973a96af48270ae
Tiago Da Silva Sousa <A238917@ae.local>
2021-11-23 15:25:22 -0300

    correcoes com plugin

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-alert(1);
-chrome.tabs.executeScript({
-    code: '(alert(2));' //argument here is a string but function.toString() returns function's code
-}, (results) => {
-    //Here we have just the innerHTML and not DOM structure
-    console.log('Popup script:')
-    console.log(results);
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.executeScript(null, {execScript});
 });
-window.document.querySelector('video').removeAttribute('disablepictureinpicture');
-window.document.querySelector('video').requestPictureInPicture();
+
+function execScript() {
+    window.document.querySelector('video').removeAttribute('disablepictureinpicture');
+    window.document.querySelector('video').requestPictureInPicture();
+}
